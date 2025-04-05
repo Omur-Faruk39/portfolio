22c8ed721947ff2b59e9b6e65025f81c04b08ab2
Omur-Faruk39 <omurfaruk39bd@gmail.com>
2025-04-05 22:54:21 +0600

    create map components

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,6 +1,4 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
-
+import Map from "./Map";
 import React from "react";
 
 function Contact() {
@@ -12,31 +10,7 @@ function Contact() {
         consectetur velit
       </p>
       <div className="flex justify-center items-center gap-5 mt-10">
-        <div className=" shadow-[0_0_5px_rgba(0,0,0,0.2)] min-w-[450px] min-h-[500px] bg-white rounded-[10px]">
-          <div className="m-6">
-            <div>
-              <div>
-                <div className="flex">
-                  <div className="w-[50px] h-[50px] bg-[#F9F9F9] rounded-full flex justify-center items-center mr-5">
-                    <FontAwesomeIcon icon={faLocationDot} />
-                  </div>
-                  <div>
-                    <h1>Address</h1>
-                    <p>Ranigonj, Goraghat, Dinajpur, Bangldesh</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14428.92153719722!2d89.22085745135395!3d25.296462565914144!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39fcc2910510bf13%3A0xd29252131889a3ae!2sRanigong%20Bazar%2C%20Ghoraghat!5e0!3m2!1sen!2sbd!4v1743699369573!5m2!1sen!2sbd"
-              width="400"
-              height="300"
-              allowfullscreen=""
-              loading="lazy"
-              referrerpolicy="no-referrer-when-downgrade"
-            ></iframe>
-          </div>
-        </div>
+        <Map />
         <div>contact</div>
       </div>
     </div>
