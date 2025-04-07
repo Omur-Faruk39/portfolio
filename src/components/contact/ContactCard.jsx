adca59b42a196c22c6b2506aa451bf0cbb57ea1e
Omur-Faruk39 <omurfaruk39bd@gmail.com>
2025-04-07 20:35:07 +0600

    active contact section

--- src/components/contact/ContactCard.jsx
+++ src/components/contact/ContactCard.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { use } from "react";
 
 function ContactCard() {
+  const [isDisabled, setIsDisabled] = React.useState(false);
   const [data, setData] = React.useState({
     name: "",
     email: "",
     subject: "",
     message: "",
+    access_key: "05aef7b9-c673-4d10-8f3e-e7db3ae68f21",
   });
 
   const changeHandler = (e) => {
@@ -13,18 +15,40 @@ function ContactCard() {
     setData((prev) => ({ ...prev, [id]: value }));
   };
 
-  const submitHandler = (e) => {
+  const submitHandler = async (e) => {
     e.preventDefault();
-    console.log("Form submitted", data);
+    setIsDisabled(true);
+    const json = JSON.stringify(data);
+    const res = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: json,
+    }).then((res) => res.json());
+
+    if (res.success) {
+      alert("Message sent successfully!");
+      setData({ ...data, name: "", email: "", subject: "", message: "" });
+      setIsDisabled(false);
+    } else {
+      alert("Failed to send message. Please try again.");
+      setIsDisabled(false);
+    }
   };
 
   return (
-    <form className=" shadow-[0_0_5px_rgba(0,0,0,0.2)] max-w-[800px] min-w-[800px] min-h-[500px] bg-white rounded-[10px]">
+    <form
+      disabled="true"
+      className=" shadow-[0_0_5px_rgba(0,0,0,0.2)] max-w-[800px] min-w-[800px] min-h-[500px] bg-white rounded-[10px]"
+    >
       <div className="flex justify-center items-center mt-10 mb-10 gap-5">
         <div className="flex flex-col gap-5 w-[45%]">
           <label htmlFor="name">You Name</label>
           <input
             type="text"
+            disabled={isDisabled}
             id="name"
             onChange={changeHandler}
             value={data.name}
@@ -35,6 +59,7 @@ function ContactCard() {
         <div className="flex flex-col gap-5 w-[45%]">
           <label htmlFor="email">You Email</label>
           <input
+            disabled={isDisabled}
             type="email"
             onChange={changeHandler}
             id="email"
@@ -46,11 +71,12 @@ function ContactCard() {
       </div>
 
       <div className="flex flex-col gap-5 w-[90%] mx-auto mb-10">
-        <label htmlFor="subject">Your Message</label>
+        <label htmlFor="subject">Subject</label>
         <input
           id="subject"
           type="text"
           onChange={changeHandler}
+          disabled={isDisabled}
           value={data.subject}
           className="border border-solid border-[rgba(0,0,0,0.4)] rounded-[4px] w-[100%] h-[40px] px-2 py-2 focus:outline-none focus:border-[rgba(52,183,167,0.5)]"
         />
@@ -60,6 +86,7 @@ function ContactCard() {
         <label htmlFor="message">Your Message</label>
         <textarea
           id="message"
+          disabled={isDisabled}
           required
           onChange={changeHandler}
           rows="7"
@@ -70,6 +97,7 @@ function ContactCard() {
 
       <div className="flex justify-center items-center w-[90%] mb-10">
         <input
+          disabled={isDisabled}
           type="submit"
           value="Send Message"
           className="px-[20px] py-[10px] rounded-full bg-[#34b7a7] text-white cursor-pointer active:bg-[#6be6d7] hover:bg-[#34b7a7] transition-all duration-[0.15s] "
